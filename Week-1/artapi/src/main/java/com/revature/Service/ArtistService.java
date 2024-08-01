9e3b42264a3671aabb92bf3528ea2f88f4a63c03
WilliamTRev <william.terry@revature.com>
2024-08-01 16:08:32 -0400

    added more comments to artapi

--- Week-1/artapi/src/main/java/com/revature/Service/ArtistService.java
+++ Week-1/artapi/src/main/java/com/revature/Service/ArtistService.java
@@ -6,29 +6,65 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+/**
+ * The @Service annotation may also be used instead of the @Component annotation, the @Service annotation is a
+ * Stereotype annotation, which would be a component with a preset configuration that would be appropriate for a
+ * Service class (in this case, @Service and @Component behave identically.)
+ */
 @Service
 public class ArtistService {
+    /**
+     * This class relies on an ArtistRepository dependency. Spring can inject this Object as a Bean (Object managed by
+     * Spring) into this class, rather than having the developer instantiate the object. This is referred to as
+     * Dependency Injection, which is a part of Spring Core.
+     */
     ArtistRepository artistRepository;
+    /**
+     * The @Autowired annotation will automatically find the most appropriate Bean to inject into this class.
+     * This class is dependent on an ArtistRepository, so @Autowired will inject an ArtistRepository bean from the IOC
+     * container (inversion-of-control.) This is referred to as Dependency Injection, which is a part of Spring Core.
+     *
+     * There are 3 ways to conduct Dependency Injection in Spring. The most common way, constructor injection, is
+     * leveraged here by placing @Autowired above the constructor. The developer may also perform Setter injection
+     * by placing @Autowired above a Setter method, or Field injection by placing the @Autowired annotation above
+     * a class's field.
+     */
 
     @Autowired
     public ArtistService(ArtistRepository artistRepository){
         this.artistRepository = artistRepository;
     }
-
+    /**
+     * Method uses the JPARepository-provided method findAll().
+     * @return a list of all Artist entities
+     */
     public List<Artist> getAllArtists(){
         return artistRepository.findAll();
     }
-
+    /**
+     * Method persists an Artist. The artist Object in the parameter is transient (ie, no relationship with a database
+     * record), and the Artist Object returned by the save() method is persistent (a relationship with a database
+     * record exists)
+     * @param artist a transient Artist entity
+     * @return a persisted Artist entity
+     */
     public Artist saveArtist(Artist artist){
         return artistRepository.save(artist);
     }
-
+    /**
+     * Method retrieves Artists with a particular name by leveraging a JPQL query written in ArtistRepository.
+     * @param name a Name for a particular painter (eg 'Picasso')
+     * @return all Artist entities with the nationality
+     */
     public List<Artist> getArtistsByName(String name){
         return artistRepository.findArtistsByName(name);
 
     }
-
+    /**
+     * Method retrieves Artists of a particular Nationality by leveraging a query method written in ArtistRepository.
+     * @param nationality a String for a nationality (eg 'Spanish')
+     * @return all Artist entities with the nationality
+     */
     public List<Artist> getArtistsByNationality(String nationality){
         return artistRepository.findArtistsByNationality(nationality);
     }
--- Week-1/artapi/src/main/java/com/revature/Service/PaintingService.java
+++ Week-1/artapi/src/main/java/com/revature/Service/PaintingService.java
@@ -10,7 +10,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+/**
+ * The @Service annotation may also be used instead of the @Component annotation, the @Service annotation is a
+ * Stereotype annotation, which would be a component with a preset configuration that would be appropriate for a
+ * Service class (in this case, @Service and @Component behave identically.)
+ *
+ * The @Transactional annotation wraps every method in this class inside a database transaction, which is a set of
+ * database statements that happen in isolation of all other database transactions. This means that multiple methods
+ * of the class can run simultaneously (Spring is multithreaded) without having an effect such as a dirty read,
+ * reading data that has been modified in an in-progress transaction. This matters when a Transaction has multiple
+ * steps. (for instance, when a single transaction should process 100 database statements for purchasing items from a
+ * user's cart, we don't want Spring to make the mistake of reading from an incomplete transaction, as e.g. a
+ * request for the total cost of a user's cart could return an erroneous amount, such as only 50 items, if Spring
+ * happens to read an incomplete transaction.)
+ */
 @Service
 @Transactional
 public class PaintingService {
@@ -23,7 +36,12 @@ public class PaintingService {
         this.artistRepository=artistRepository;
 
     }
-
+    /**
+     * Leverage the Spring Data JPARepository method save() to persist a new Painting to the Painting table. The
+     * new painting is returned. This functionality is provided to allow you to test your API.
+     * @param painting A transient Painting entity
+     * @return the persisted Painting entity, if successful
+     */
     public Painting savePainting(long artistID, Painting painting){
         Artist a = artistRepository.findById(artistID).get();
         a.getPaintings().add(painting);
